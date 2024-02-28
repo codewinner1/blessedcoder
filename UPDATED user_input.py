34f7d087fde76c11b0bdf9ca6dca9e8f69a425d9
Mwanamisi <mwanamisimwaka001@gmail.com>
2024-02-28 16:12:35 +0300

    UPDATED user_input.py

---
+++ UPDATED user_input.py
@@ -0,0 +1,10 @@
+#enter name and assign it to variable name
+name = (input('enter your name:'))
+
+#assign age to variable age
+age = (input('enter your age:'))
+
+#assign location to variable location
+location = (input('enter where you live:'))
+
+print('Hello',name,'you are',age,'years old and you live in',location,)
