2083e7a868ccfe80d51693ac58b9911c84485d4d
Mwanamisi <mwanamisimwaka001@gmail.com>
2024-02-28 14:00:09 +0300

    python file update

---
+++ assign.py
@@ -0,0 +1,2 @@
+name = ('testing')])
+print(len(name))
---
+++ user_input.py
@@ -0,0 +1,15 @@
+x = input('What is your name?')
+print(x)
+# assign a variable to name
+name = ('Mwanamisi Swaleh')
+
+y = input('How old are you?')
+print(y)
+#assign a variable to age
+age = 24
+
+input('Where do you stay?')
+location = ('Nairobi')
+
+input('Hello!', end= " ")
+print('Mwanamisi Swaleh,you are 24 years old and live in Nairobi')
